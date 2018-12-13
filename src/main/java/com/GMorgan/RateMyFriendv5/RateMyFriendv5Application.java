350233af14bd23ce7d94136da0de4bf9d404a872
Rahmi Pruitt <rjpruitt16@gmail.com>
2018-12-13 07:19:13 -0700

    Hello World
    A spring boot project configured with JPA, h2, lombok. web.

---
+++ src/main/java/com/GMorgan/RateMyFriendv5/RateMyFriendv5Application.java
@@ -0,0 +1,14 @@
+package com.GMorgan.RateMyFriendv5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateMyFriendv5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateMyFriendv5Application.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/GMorgan/RateMyFriendv5/RateMyFriendv5ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.GMorgan.RateMyFriendv5;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RateMyFriendv5ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
