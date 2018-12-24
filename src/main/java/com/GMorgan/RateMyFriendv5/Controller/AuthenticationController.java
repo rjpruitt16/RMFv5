4b88d02a1f3aa47f3840544055aa0cc7595abba4
Rahmi Pruitt <rjpruitt16@gmail.com>
2018-12-23 19:47:04 -0700

    Created an Authentication Controller pt.2
    Testing the controller in the browser, and it is working. Next, I will write a test.

--- src/main/java/com/GMorgan/RateMyFriendv5/Controller/AuthenticationController.java
+++ src/main/java/com/GMorgan/RateMyFriendv5/Controller/AuthenticationController.java
@@ -3,7 +3,7 @@ package com.GMorgan.RateMyFriendv5.Controller;
 import com.GMorgan.RateMyFriendv5.Service.UserService;
 import com.GMorgan.RateMyFriendv5.Utils.Mappings;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,13 +14,14 @@ public class AuthenticationController {
     @Autowired
     private UserService service;
 
-    @GetMapping(Mappings.LOGIN)
+    @RequestMapping(Mappings.LOGIN)
     @ResponseBody
-    public String login(@RequestParam String usernameOrEmail, @RequestParam String password) {
-       boolean isSuccessful = service.login(usernameOrEmail, password);
-       if (isSuccessful) {
+    public String login(@RequestParam("userId") String usernameOrEmail,
+                        @RequestParam("password") String password) {
+        boolean isSuccessful = service.login(usernameOrEmail, password);
+        if (isSuccessful) {
            return "Login Successful";
-       }
-       return "Login is unsuccessful";
+        }
+        return "Login is unsuccessful";
     }
 }
--- src/main/java/com/GMorgan/RateMyFriendv5/RateMyFriendv5Application.java
+++ src/main/java/com/GMorgan/RateMyFriendv5/RateMyFriendv5Application.java
@@ -1,6 +1,5 @@
 package com.GMorgan.RateMyFriendv5;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +10,5 @@ public class RateMyFriendv5Application {
 	public static void main(String[] args) {
 		SpringApplication.run(RateMyFriendv5Application.class, args);
 	}
-
 }
 
