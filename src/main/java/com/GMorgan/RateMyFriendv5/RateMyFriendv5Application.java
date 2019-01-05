69790a48c2841151486f8a23d09aa997fd9c5403
Rahmi Pruitt <rjpruitt16@gmail.com>
2019-01-05 13:13:06 -0700

    Adding Swagger to rest api pt.1-2
    Change the request mappings to get and post mappings for swagger.
    Configured swagger for documentation and testing purposes.

--- src/main/java/com/GMorgan/RateMyFriendv5/RateMyFriendv5Application.java
+++ src/main/java/com/GMorgan/RateMyFriendv5/RateMyFriendv5Application.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.GMorgan.RateMyFriendv5.Service",
-		"com.GMorgan.RateMyFriendv5.Controller"})
+		"com.GMorgan.RateMyFriendv5.Controller",
+		"com.GMorgan.RateMyFriendv5.Config"})
 public class RateMyFriendv5Application {
 	public static void main(String[] args) {
 		SpringApplication.run(RateMyFriendv5Application.class, args);
